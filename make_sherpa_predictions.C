ac963b1f580ade817caeec219ce742d1d5912199
peruzzim <peruzzim@server.fake>
2014-12-15 19:54:44 +0100

    graphics and ratio plots

--- make_sherpa_predictions.C
+++ make_sherpa_predictions.C
@@ -1,6 +1,7 @@
 #include "binsdef.h"
 #include "TFile.h"
 #include "TH1F.h"
+#include "TF1.h"
 #include <iostream>
 #include "TCanvas.h"
 #include "TStyle.h"
@@ -11,6 +12,13 @@
 bool dolog = false;
 
 float scale_sherpa = 16.2/13.8; // = +17%
+
+TPad* newPad(int number, TString name, float x1, float y1, float x2, float y2){
+  TPad *p = new TPad(name.Data(),name.Data(),x1,y1,x2,y2);
+  p->SetNumber(number);
+  p->Draw();
+  return p;
+}
 
 void make_sherpa_predictions_(TString filename = "outphoton/outphoton_effunf_sig_Default.root", TString var="", bool withdata = false, bool with42data = false){
 
@@ -58,12 +66,20 @@ void make_sherpa_predictions_(TString filename = "outphoton/outphoton_effunf_sig
 
     TString unit = diffvariables_units_list(diffvariable);
     hi->GetXaxis()->SetTitle(Form("%s %s",diffvariables_names_list(diffvariable).Data(),unit!=TString("") ? (TString("(").Append(unit.Append(")"))).Data() : TString("").Data()));
-
+    hi->GetYaxis()->SetTitle(Form("d#sigma/d%s (pb/%s)",diffvariables_names_list(diffvariable).Data(),unit!=TString("") ? (TString("(").Append(unit.Append(")"))).Data() : TString("1").Data()));
     float max = hi->GetMaximum();
     hi->GetYaxis()->SetRangeUser(0,1.3*max);
   
     TCanvas *c = new TCanvas();
     c->cd();
+    newPad(1,"pad1",0,0.3,1,1);
+    newPad(2,"pad2",0,0,1,0.3);
+    c->cd(1);
+
+    hi->GetXaxis()->SetTitleSize(0.05);
+    hi->GetYaxis()->SetTitleSize(0.05);
+    hi->GetXaxis()->SetLabelSize(0.04);
+
     hi->Draw();
     if (dolog) hi->GetYaxis()->UnZoom();
     c->Update();
@@ -82,7 +98,7 @@ void make_sherpa_predictions_(TString filename = "outphoton/outphoton_effunf_sig
 	hdata42->SetMarkerStyle(25);	
 	hdata42->SetMarkerColor(kRed);
 	hdata42->SetLineColor(kRed);
-	c->cd();
+	c->cd(1);
 	hdata42->Draw("same E1");
 	hdata42->Print();
 	max = (max<hdata42->GetMaximum()) ? hdata42->GetMaximum() : max;
@@ -90,7 +106,7 @@ void make_sherpa_predictions_(TString filename = "outphoton/outphoton_effunf_sig
 
       TH1F *hdata = (TH1F*)(fdata->Get(Form("histo_finalxs_fortheorycomp_%s",it->Data())));
       hdata->SetMarkerStyle(20);
-      c->cd();
+      c->cd(1);
       hdata->Draw("same P");
       hdata->Print();
       max = (max<hdata->GetMaximum()) ? hdata->GetMaximum() : max;
@@ -98,7 +114,28 @@ void make_sherpa_predictions_(TString filename = "outphoton/outphoton_effunf_sig
       hi->GetYaxis()->SetRangeUser(0,1.3*max);
       if (dolog) hi->GetYaxis()->UnZoom();
 
-      addCMS(c);
+
+      TH1F *ratio = (TH1F*)(hdata->Clone("ratio"));
+      ratio->Divide(hi);
+      ratio->SetMarkerStyle(1);
+      ratio->GetYaxis()->SetTitle("Data / SHERPA");
+      ratio->GetXaxis()->SetTitle("");
+      ratio->GetXaxis()->SetLabelSize(0.09);
+      ratio->GetYaxis()->SetTitleSize(0.12);
+      ratio->GetYaxis()->SetTitleOffset(0.5);
+      ratio->GetYaxis()->SetLabelSize(0.09);
+
+
+      addCMS((TPad*)(c->GetPad(1)));
+
+      c->cd(2);
+      ((TPad*)(c->GetPad(2)))->SetLogy(0);
+      ratio->GetYaxis()->SetRangeUser(0,3);
+      ratio->Draw("E1");
+      TF1 *line = new TF1("line","1",ratio->GetXaxis()->GetXmin(),ratio->GetXaxis()->GetXmax());
+      line->SetLineColor(kBlue);
+      line->Draw("same");
+      ratio->Draw("E1 same");
 
       c->Update();
       c->SaveAs( Form("plots/SHERPApred_%s%s_withdata.pdf",it->Data(),lstring.Data()));
