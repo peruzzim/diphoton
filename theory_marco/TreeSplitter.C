722f5bdab967fd4b7df57b8ff343fdee04e0eb47
peruzzim <peruzzim@server.fake>
2015-02-14 10:14:38 +0100

    bugfix

--- theory_marco/TreeSplitter.C
+++ theory_marco/TreeSplitter.C
@@ -1,5 +1,7 @@
 #include "TTree.h"
 #include "TFile.h"
+#include <iostream>
+using namespace std;
 
 class TreeSplitter {
 
@@ -11,6 +13,7 @@ public:
   TTree *newtree;
 
   Long64_t nentries;
+  Long64_t thisentries;
   int counter_files;
   int nparts_;
 
@@ -20,12 +23,14 @@ public:
   TreeSplitter(TString filename, TString treename, int nparts) : nparts_(nparts)
   {
     counter_files = 0;
+    thisentries = 0;
     newtree = 0;
     newfile = 0;
     oldfile = new TFile(filename.Data(),"read");
     oldtree = (TTree*)(oldfile->Get(treename.Data()));
     nentries = oldtree->GetEntries();
     oldtree->SetBranchAddress("ntrack",&ntrack,&b_ntrack);
+    donewfile();
     splitter();
   };
 
@@ -33,17 +38,20 @@ public:
     if (newtree){
       newtree->Print();
       newtree->AutoSave();
+      newfile->Close();
     }
     counter_files++;
+    thisentries = 0;
     newfile = new TFile(Form("splitted_%d.root",counter_files),"recreate");
     newfile->cd();
     newtree = oldtree->CloneTree(0);
   }
 
   void splitter(){
-    Long64_t thisentries = 0;
+
     int prev_ntrack = -1;
     for (Long64_t i=0;i<nentries; i++) {
+      if (i%1000000==0) cout << "Processing entry " << i << endl;
       oldtree->GetEntry(i);
       if (ntrack>prev_ntrack && thisentries>=nentries/nparts_) donewfile();
       thisentries++;
